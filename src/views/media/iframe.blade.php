cbef0cf0cb864959d9053ad909ce52e365dac42c
maxime <maxime@imprimerie-roue.fr>
2014-10-13 10:32:51 +0200

    progress bar ok

--- src/views/media/iframe.blade.php
+++ src/views/media/iframe.blade.php
@@ -1,35 +1,52 @@
 <div class="row">
 	@include('media::media.upload-form',['alias'=>$alias])
 	<hr />
+   <div class="progress">
+    <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+       <span class="bitrate"></span>
+       <span class="percent"></span>
+      <span class="sr-only">0% Complete (success)</span>
+    </div>
+  </div>
+  <hr />
 	@include('media::media.model-file-list',['alias'=>$alias])
 </div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="/js/vendor/jquery.ui.widget.js"></script>
+
 <script src="/js/jquery.iframe-transport.js"></script>
 <script src="/js/jquery.fileupload.js"></script>
+<script src="/js/jquery.fileupload-process.js"></script>
+
 <script>
-  var percent = $('.percent');
-  var bar = $('.bar');
+
+  // var percent = $('.percent');
+  // var bar = $('.bar');
+  var progressbar = $('.progress .progress-bar');
 
   $('#formUpload').fileupload({
-      formData : $('#media-upload-alias-info').serializeArray(),
-      dropZone: $('#dropzone'),
-      apc: true,
-     
-     complete: function(data) {
+   formData : $('#media-upload-alias-info').serializeArray(),
+   dropZone: $('#dropzone'),
+     complete: function(data){
         $file_img = $.parseJSON(data.responseText);
         $('#media-list tr.media-th:first').after($file_img.template);
+        progressbar.css('width',0+'%').attr('aria-valuenow',0);
+        $('.percent').text('');
+        $('.bitrate').text('');
      },
-     error: function(response)
-     {
+     error: function(response){
         var r = $.parseJSON(response.responseText);
         alert(r.error.message);
      }
     }).bind('fileuploadprogress',function(e, data){
-    var progress = parseInt(data.loaded / data.total * 100, 10);
-    console.log(progress);
+       var progress = parseInt(data.loaded / data.total * 100, 10);
+        progressbar.css('width',progress + '%').attr('aria-valuenow',progress);
+        kps = Math.floor(data.bitrate/1000);
+        $('.percent').text(progress+ "%");
+        $('.bitrate').text(kps+" ko/s");
     });
+
 
 $(document).bind('dragover', function (e) {
     var dropZone = $('#dropzone'),
@@ -58,6 +75,4 @@ $(document).bind('dragover', function (e) {
         dropZone.removeClass('in hover');
     }, 100);
 });
-
-
 </script>
